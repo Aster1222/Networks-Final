65dccaa94c8fdbb6f731b913bffbb6e8c7e65518
rohitmusti <rohit.musti.rm@gmail.com>
2018-10-27 17:05:58 -0400

    it now converts a single character to its binary, next step is making the packet

--- TCP.py
+++ TCP.py
@@ -1,5 +1,8 @@
 from socket import *
+# import soundcard
+# import numpy
 
+# import pyaudio
 
 serverPort = 4000
 tcpServerSocket = socket(AF_INET, SOCK_STREAM)
@@ -10,10 +13,11 @@ tcpServerSocket.listen(1)
 
 print("IP Address: " + str(hostIP))
 
-
 while 1:
 	conn, addr = tcpServerSocket.accept()
 	data = conn.recv(1024)
 	print("connect with " + str(addr[0]) + " on port" + str(addr[1]))
-	print(data.decode())
+	print("binary of single letter transmitted it: " + str(bin(ord(data.decode()[0])))[2:])
+
+
 
