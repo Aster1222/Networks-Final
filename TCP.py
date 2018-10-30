38158ae2d4ec68f03afd63eaa646e00e7cba9810
rohitmusti <rohit.musti.rm@gmail.com>
2018-10-30 14:41:57 -0400

    now you can send multiple commands and exit

--- TCP.py
+++ TCP.py
@@ -13,11 +13,17 @@ tcpServerSocket.listen(1)
 
 print("IP Address: " + str(hostIP))
 
+conn, addr = tcpServerSocket.accept()
+print("connect with " + str(addr[0]) + " on port" + str(addr[1]))
+
 while 1:
-	conn, addr = tcpServerSocket.accept()
 	data = conn.recv(1024)
-	print("connect with " + str(addr[0]) + " on port" + str(addr[1]))
 	print("binary of single letter transmitted it: " + str(bin(ord(data.decode()[0])))[2:])
+
+	if (data.decode().lower().strip() == "exit"):
+		conn.close()
+		break
+
 
 
 
