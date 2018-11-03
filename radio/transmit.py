305e715bb6837a3a1247f8aacd5f75ee2bdf5782
collinsrhuffiii <collinsrhuffiii@gmail.com>
2018-11-03 17:14:05 -0400

    ipynb to python files

--- radio/transmit.py
+++ radio/transmit.py
@@ -1,5 +1,16 @@
+
+# coding: utf-8
+
+# In[27]:
+
+get_ipython().run_line_magic('pylab', '')
 import sounddevice as sd
 import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[28]:
 
 def encode_manchester(bits):
     manchester = np.array([[False, True] if b else [True, False] for b in bits])
@@ -7,39 +18,70 @@ def encode_manchester(bits):
     return manchester
 
 
-def generate_am_signal(manchester_encoding, samp_per_bit, num_samples, samp_rate):
+# In[29]:
+
+def generate_am_signal(manchester_encoding, frequency, samp_per_bit, num_samples, samp_rate):
     M = np.tile(manchester_encoding,(1,int(samp_per_bit)))
     t = r_[0.0:2*num_samples]/samp_rate
-    am_signal = M.ravel()*np.sin(2*np.pi*f0*t)
+    am_signal = M.ravel()*np.sin(2*np.pi*frequency*t)
     return am_signal, t
 
 
-def transmit(data, samp_rate, baud, frequency, len_preamble):
-    payload = np.array([(s == '1') for s in data])
+# In[30]:
+
+def transmit(data, samp_rate, baud, frequency, len_preamble, packet_id='11111111', length=44):
+    preamble = ''.join('1' for _ in range(8))
+    len_packet = '00' + str(bin(length))[2:]
+    components = [preamble, packet_id, len_packet, data]
+    grid = []
+    for component in components:
+        grid.append([int(d) for d in component])
+    grid = np.array(grid)
+    print(grid)
+    
+    col_parity = grid.sum(axis=0) % 2
+    row_parity = grid.sum(axis=1) % 2
+    
+    packet = [d == 1 for d in np.nditer(grid)]
+    packet += [d == 1 for d in col_parity]
+    packet += [d == 1 for d in row_parity]
+    packet = np.array(packet).reshape(-1,1)
+    
     samp_per_bit = samp_rate/baud
-    len_packet =  len_preamble + len(data)
-    num_samples = len_packet * samp_per_bit
-    preamble = np.array([[True] if i % 2 == 0 else [False] for i in range(len_preamble)])
-    bits = np.concatenate((preamble, data))
-    manchester = encode_manchester(bits)
-    am_signal, t = generate_am_signal(manchester, samp_per_bit, num_samples, samp_rate)
+    num_samples = length * samp_per_bit
+    manchester = encode_manchester(packet)
+    am_signal, t = generate_am_signal(manchester, frequency, samp_per_bit, num_samples, samp_rate)
     sd.play(am_signal, blocking=True)
-    return bits, am_signal, t
+    return packet, am_signal, t
 
 
-if __name__ == '__main__':
-    samp_rate = 44100  # sampling rate
-    baud = 300  # symbol rate
-    len_preamble = 10
-    len_data = 10  # number of bits
-    frequency = int(88.1e6)
-    # data = randn(len_data,1) > 0
+# In[31]:
 
-    bits, am_signal, t = transmit(data, samp_rate, baud, frequency, len_preamble)
-    output = []
-    for b in bits:
-        if b:
-            output.append(1)
-        else:
-            output.append(0)
-    print(output)
+samp_rate = 44100  # sampling rate
+baud = 300  # symbol rate
+len_preamble = 10
+frequency = int(88.1e6)
+data ='11001011'
+
+bits, am_signal, t = transmit(data, samp_rate, baud, frequency, len_preamble)
+output = []
+for b in bits:
+    if b:
+        output.append(1)
+    else:
+        output.append(0)
+print(output)
+fig = figure(figsize = (16,4))
+plot(t, am_signal)
+xlabel('time [s]')
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
